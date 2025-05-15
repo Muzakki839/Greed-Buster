8af68ca8244d40a31aa3fe32f43f8b04bb5aa84d
Muzakki839 <muzakki839@gmail.com>
2025-05-15 13:39:47 +0700

    Add UI Prefabs for High Score, Score, and Timer

--- Assets/Scripts/Core/Timer.cs
+++ Assets/Scripts/Core/Timer.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float timeLimit = 10f;
+    [SerializeField] private Slider slider;
+    [SerializeField] private bool playOnStart = false;
+
+    private float timeRemaining;
+    private bool isTimerRunning = false;
+
+    private void Start()
     {
-        
+        timeRemaining = timeLimit;
+        slider.maxValue = timeLimit;
+        slider.value = timeRemaining;
+
+        if (playOnStart)
+        {
+            StartTimer();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isTimerRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+            slider.value = timeRemaining;
+
+            if (timeRemaining <= 0)
+            {
+                StopTimer();
+                timeRemaining = 0;
+                Debug.Log("Time's up!");
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+        timeRemaining = timeLimit;
+    }
+
+    public void StopTimer()
+    {
+        isTimerRunning = false;
     }
 }
